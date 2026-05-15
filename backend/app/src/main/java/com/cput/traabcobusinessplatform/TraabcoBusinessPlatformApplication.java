c26a2d15d5479af2a42072d973da63c3c402d8a0
Muso Nkuntsu <231223722@mycput.ac.za>
2026-05-15 16:56:40 +0200

    Fix backend structure - properly track springboot project

---
+++ backend/app/src/main/java/com/cput/traabcobusinessplatform/TraabcoBusinessPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.cput.traabcobusinessplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraabcoBusinessPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraabcoBusinessPlatformApplication.class, args);
+    }
+
+}
---
+++ backend/app/src/test/java/com/cput/traabcobusinessplatform/TraabcoBusinessPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cput.traabcobusinessplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraabcoBusinessPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
